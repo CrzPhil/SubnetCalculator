285ffea835039b500fc311658c1d7319364e1fce
CrzPhil <philippos.g@me.com>
2022-06-14 23:28:47 +0100

    both subnet mask options done and dusted

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ BANNER = """ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄ 
 """
 
 
+def shToDec(sh: int):
+    if 0 <= sh <= 32:
+        if sh % 8 == 0:
+            return 255
+
+        out = 0
+        j = 128
+
+        for i in range(sh % 8):
+            out += j
+            j //= 2
+        return out
+    else:
+        return -1
+
+
 def getSubnetByShorthand():
     sh = "bob"
     while sh[0] != '/' and 0 < len(sh) < 4:
@@ -19,11 +35,44 @@ def getSubnetByShorthand():
         except ValueError:
             print("Invalid shorthand. Stick to the format '/22' '/15' '/5' '/17', etc.")
             return
-    pass
+
+    if sh <= 8:
+        prefix = ""
+        suffix = ".0.0.0"
+    elif sh <= 16:
+        prefix = "255."
+        suffix = ".0.0"
+    elif sh <= 24:
+        prefix = "255.255."
+        suffix = ".0"
+    else:
+        prefix = "255.255.255."
+        suffix = ""
+
+    return prefix + str(shToDec(sh)) + suffix
 
 
-def getSubnetByDecimals(decimals):
-    pass
+def getSubnetByDecimals():
+    decimals = input("Subnet mask (xyz.xyz.xyz.xyz): ")
+    aces = decimals.count("255")
+
+    if aces == 4:
+        return "/32"
+
+    dec = int(decimals.split('.')[aces])
+
+    if dec < 128:
+        return -1
+
+    tmp = 128
+    out = 0
+
+    while dec != 0:
+        dec -= tmp
+        out += 1
+        tmp //= 2
+
+    return f"/{out}"
 
 
 def getSubnetAll(addressRange):
@@ -43,9 +92,9 @@ def main():
         option = input("Option: ")
 
         if option == "1":
-            getSubnetByShorthand()
+            print(getSubnetByShorthand())
         elif option == "2":
-            getSubnetByDecimals()
+            print(getSubnetByDecimals())
         elif option == "3":
             getSubnetAll()
         elif option == "E" or option == "e":
